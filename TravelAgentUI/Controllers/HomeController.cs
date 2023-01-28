31b3687c7e6f73262bd37d68fee6d4f3c3e950f6
Süleyman Samet Kaya <ssametkaya52@gmail.com>
2023-01-29 00:28:53 +0300

    Initial commit

--- TravelAgentUI/Controllers/HomeController.cs
+++ TravelAgentUI/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace TravelAgentUI.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(); 
         }
 
         public IActionResult Privacy()
